80564235e5a1da9574d8e82a6547e14c2edcecf5
jasoneffendi <jasoneffendi27@gmail.com>
2017-11-09 14:01:33 +0700

    Crud Articles, find by category, find by authour. auth delete and edit Done

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -22,6 +22,26 @@ class articleCtrl{
         })
     }
 
+    static getByAuthor(req,res) {
+        Article.find({author: req.params.id})
+        .populate('author', 'username')
+        .then((result,err) => {
+            if(err) return res.send(err)
+
+            res.send(result)
+        })
+    }
+
+    static getByCategory(req,res) {
+        Article.find({category: req.params.category})
+        .populate('author', 'username')
+        .then((result,err) => {
+            if(err) return res.send(err)
+
+            res.send(result)
+        })
+    }
+
     static post(req,res) {
         var opentoken = jwt.verify(req.body.token, 'press')
 
@@ -37,6 +57,48 @@ class articleCtrl{
             if(err) return res.send(err)
 
             res.send(result)
+        })
+    }
+
+    static put(req,res) {
+        var opentoken = jwt.verify(req.body.token, 'press')
+        Article.findOne({_id: req.params.id})
+        .then((result,err) => {
+            if(err) return res.send(err)
+  
+            if(result.author == opentoken.id) {
+                Article.findOneAndUpdate({_id: req.params.id}, {
+                    title: req.body.title,
+                    content: req.body.content,
+                    category: req.body.category
+                })
+                .then((result,err) => {
+                    if(err) return res.send(err)
+    
+                    res.send(result)
+                })
+            } else {
+                return res.send('You are not authorized to edit this article')                
+            }
+        })
+    }
+
+    static del(req,res) {
+        var opentoken = jwt.verify(req.body.token, 'press')
+        Article.findOne({_id: req.params.id})
+        .then((result,err) => {
+            if(err) return res.send(err)
+
+            if(result.author == opentoken.id) {
+                Article.findOneAndRemove({_id: req.params.id})
+                .then((result,err) => {
+                    if(err) return res.send(err)
+    
+                    res.send(result)
+                })
+            } else {
+                return res.send('You are not authorized to edit this article')                
+            }
         })
     }
     
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,14 @@ router.get('/', articleCtrl.get);
 
 router.get('/:id/details', articleCtrl.getOne)
 
+router.get('/:id/author', articleCtrl.getByAuthor)
+
+router.get('/:category/category', articleCtrl.getByCategory)
+
+router.put('/:id', articleCtrl.put)
+
+router.delete('/:id', articleCtrl.del)
+
 router.post('/', articleCtrl.post)
 
 module.exports = router;
