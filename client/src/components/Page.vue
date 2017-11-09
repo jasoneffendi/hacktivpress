fa7c095f6b3a3b0adccf24b9a36d16bbf4cc22a2
jasoneffendi <jasoneffendi27@gmail.com>
2017-11-09 16:54:00 +0700

    Search by author, id is still hardcoded

--- client/src/components/Page.vue
+++ client/src/components/Page.vue
@@ -3,6 +3,13 @@
 <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search by category" ref='category'>
       <button class="btn btn-secondary my-2 my-sm-0" @click.prevent="getByCategory()">Search</button>
+      <div class="form-group">
+      <label for="exampleSelect1">Author</label>
+      <select class="form-control" id="exampleSelect1" ref="author">
+        <option value="5a03f542fa744033f9366354">jason</option>
+      </select>
+      </div>
+      <button class="btn btn-secondary my-2 my-sm-0" @click.prevent="getByAuthor()">Get by author</button>
       <button class="btn btn-secondary my-2 my-sm-0" @click.prevent="getArticles()">Get all</button>
 </form>
     <router-view
@@ -36,6 +43,17 @@ export default {
     },
     getByCategory () {
       this.$http.get('/' + this.$refs.category.value + '/category')
+      .then(({data}) => {
+        console.log(data)
+        this.content = data
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    getByAuthor () {
+      console.log(this.$refs.author.value)
+      this.$http.get('/' + this.$refs.author.value + '/author')
       .then(({data}) => {
         console.log(data)
         this.content = data
